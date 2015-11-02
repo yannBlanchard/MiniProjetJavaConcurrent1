f335306db66714f202a59e0665c6d3e6e137c82b
yannBlanchard <yann.77.blanchard@gmail.com>
2015-11-02 16:37:59 +0100

    modif testSaisie

--- src/testSaisie.java
+++ src/testSaisie.java
@@ -1,5 +1,6 @@
+import javax.swing.*;
 
-public class testSaisie {
+public class testSaisie extends JFrame{
 
 	public static void main(String[] args) {
 		SimpleFenetre fenetreAff = new SimpleFenetre("Main Affichage");
